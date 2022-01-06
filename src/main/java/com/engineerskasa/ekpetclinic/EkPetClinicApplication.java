d00ea271f7beffab32034bd6482884faf7110d71
dedongh <slimshades68@gmail.com>
2022-01-06 22:06:07 +0000

    initial commit

---
+++ src/main/java/com/engineerskasa/ekpetclinic/EkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.engineerskasa.ekpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/engineerskasa/ekpetclinic/EkPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineerskasa.ekpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
